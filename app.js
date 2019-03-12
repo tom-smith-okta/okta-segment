deaa2734d38ed668b4e897b3757b1123d1a84b21
Tom Smith <tom.smith@okta.com>
2019-03-12 11:09:29 -0400

    upgrade

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
+
 var fs = require('fs')
-// var bodyParser = require('body-parser')
-// var request = require('request');
-// var session = require("express-session");
 
 const app = express()
 
 app.use(express.static('public'))
-// app.use(bodyParser.json())
-// var urlencodedParser = bodyParser.urlencoded({ extended: false, limit: 10000000, parameterLimit: 100000 });
-// app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true, cookie: { httpOnly: false, maxAge: parseInt(process.env.COOKIE_MAX_AGE) }}))
 
 var port = process.env.PORT || 3090
 
@@ -30,100 +25,8 @@ app.get('/', function (req, res) {
 
 		page = page.replace(/{{OKTA_TENANT}}/g, process.env.OKTA_TENANT)
 		page = page.replace(/{{OKTA_CLIENT_ID}}/g, process.env.OKTA_CLIENT_ID)
-		page = page.replace(/{{OKTA_REDIRECT_URI}}/g, process.env.OKTA_REDIRECT_URI)
-		// page = page.replace("{{logo}}", process.env.OKTA_LOGO);
+		page = page.replace(/{{REDIRECT_URI}}/g, process.env.REDIRECT_URI)
 
 		res.send(page)
 	})
 })
-
-// app.post('/setSession', urlencodedParser, function (req, res) {
-
-// 	console.log("The idToken is: " + req.body.idToken)
-
-// 	var options = {
-// 		method: 'POST',
-// 		url: process.env.OKTA_TENANT + '/oauth2/v1/introspect',
-// 		qs: {
-// 			token: req.body.idToken,
-// 			token_type_hint: 'id_token',
-// 			client_id: process.env.OKTA_CLIENT_ID,
-// 			client_secret: process.env.OKTA_CLIENT_SECRET
-// 		},
-// 		headers: {
-// 			'Cache-Control': 'no-cache',
-// 			'Content-Type': 'application/x-www-form-urlencoded',
-// 			Accept: 'application/json'
-// 		}
-// 	}
-
-// 	request(options, function (error, response, body) {
-// 		if (error) throw new Error(error)
-
-// 		var obj = JSON.parse(body)
-
-// 		if (obj.active === true) {
-// 			console.log("the id token is valid.")
-
-// 			console.dir(obj)
-
-// 			req.session.okta_user_id = obj.sub
-
-// 			console.log("the user id is: " + req.session.okta_user_id)
-
-// 			res.send({result: "successfully set session"})
-
-// 		}
-// 	})
-
-// })
-
-// app.post('/syncRecords', urlencodedParser, function (req, res) {
-
-// 	// ** Important: in production, you need to check the 
-// 	// transactionID here against the Acuant server
-
-// 	console.log("The body is: " + JSON.stringify(req.body))
-// 	console.dir(req.body)
-
-// 	var profile = {}
-
-// 	for (var prop in req.body) {
-// 		console.dir("the value of " + prop + " is: " + req.body[prop])
-
-// 		profile[prop] = req.body[prop]
-// 	}
-
-// 	console.log("the user id is: " + req.session.okta_user_id)
-
-// 	if (req.body.acuant_authentication_result === "Passed") {
-
-// 		var url = process.env.OKTA_TENANT + '/api/v1/users/' + req.session.okta_user_id
-
-// 		console.log("the url is: " + url)
-
-// 		var options = {
-// 			method: 'POST',
-// 			url: url,
-// 			headers: {
-// 				'Cache-Control': 'no-cache',
-// 				Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
-// 				Accept: 'application/json',
-// 				'Content-Type': 'application/json'
-// 			},
-// 			body: {
-// 				profile: profile
-// 			},
-// 			json: true
-// 		}
-
-// 		request(options, function (error, response, body) {
-// 			if (error) throw new Error(error)
-
-// 			console.log(body)
-
-// 			res.send({msg: "successfully updated user record"})
-
-// 		})
-// 	}
-// })
